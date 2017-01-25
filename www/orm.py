3974cf5dac3e4120274b31420bb92ec74cc494e2
dq5070410 <dq5070410@126.com>
2017-01-25 10:43:31 +0800

    extend orm

--- www/orm.py
+++ www/orm.py
@@ -1,4 +1,4 @@
-#! /usr/bin/env python3
+#! /usr/bin/env python3.4
 # -*- coding:utf-8 -*-
 
 import asyncio,logging
@@ -120,3 +120,50 @@ class ModelMetaclass(type):
 		attrs['__update__'] = 'update `%s` set %s where `%s`=?' % (tableName, ', '.join(map(lambda f: '`%s`=?' % (mappings.get(f).name or f), fields)), primaryKey)
 		attrs['__delete__'] = 'delete from `%s` where `%s`=?' % (tableName, primaryKey)
 		return type.__new__(cls, name, bases, attrs)
+
+class Model(dict,metaclass=ModelMetaclass):
+	def __init__(self,**kw):
+		super(Model,self).__init__(**kw)
+	def __getattr__(self,key):
+		try:
+			return self[key]
+		except KeyError:
+			raise AttributeError(r"'Model' object has no attribute '%s'" % key)
+	def __setattr__(self,key,value):
+		self[key] = value
+	def getValueOrDefault(self,key):
+		value = getattr(self,key,None)
+		if value is None:
+			field = self.__mappings__[key]
+			if field.default is not None:
+				value = field.default() if callable(field.default) else field.default
+				logging.debug('using default value for %s:%s' % (key,str(value)))
+				setattr(self,key,value)
+		return value
+	@classmethod
+	@asyncio.coroutine
+	def findAll(cls,where=None,args=None,**kw):
+		'find objects by where clause'
+		sql = [cls.__select__]
+		if where:
+			sql.append('where')
+			sql.append(where)
+		if args is None:
+			args = []
+		orderBy = kw.get('orderBy',None)
+		if orderBy:
+			sql.append('order by')
+			sql.append(orderBy)
+		limit = kw,get('limit',None)
+		if limit is not None:
+			sql.append('limit')
+			if isinstance(limit,int):
+				sql.append('?')
+				args.append(limit)
+			elif isinstance(limit,tuple) and len(limit) == 2:
+				sql.append('?,?')
+				args.extend(limit)
+			else:
+				raise ValueError('Invalid limit value:%s' % str(limit))
+		rs = yield from select(' '.join(sql),args)
+		return [cls(**r) for r in rs]
