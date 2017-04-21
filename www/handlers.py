62b649a40e00e817f038bd360545c77b802d58e8
yanghang <dq5070410@126.com>
2017-04-21 21:58:59 +0800

    add cookie

--- www/handlers.py
+++ www/handlers.py
@@ -5,7 +5,52 @@
 
 import re,time,json,logging,hashlib,asyncio
 
+import markdown2
+from aiohttp import web
+from coroweb import get,post
+from apis import APIValueError,APIResourceNotFoundError
+
 from models import User,Comment,Blog,next_id
+from config import configs
+
+COOKIE_NAME = "awesession"
+_COOKIE_KEY = configs.session.secret
+
+def user2cookie(user,max_age):
+	'''
+	Generate cookie str by user
+	'''
+	expires = str(int(time.time() + max_age))
+	s = '%s-%s-%s-%s' % (user.id,user.passwd,expires,_COOKIE_KEY)
+	L = [user.id,expires,hashlib,sha1(s.encode('utf-8')).hexdigest()]
+	return '-'.join(L)
+
+@asyncio.coroutine
+def cookie2user(cookie_str):
+	'''
+	Parse cookie and load user if cookie is valid
+	'''
+	if not cookie_str:
+		return None
+	try:
+		L = cookie_str.split('-')
+		if len(L) != 3:
+			return None
+		uid,expires,sha1 = L
+		if int(expires) < time.time():
+			return None
+		user = yield from User.find(uid)
+		if user is None:
+			return None
+		s = '%s-%s-%s-%s' % (uid,user.passwd,expires,_COOKIE_KEY)
+		if sha1 = != hashlib.sha1(s.encode('utf-8')).hexdigest():
+			logging.info('invalid sha1')
+			return None
+		user.passwd = '******'
+		return user
+	except Exception as e:
+		logging.exception(e)
+		return None
 
 @get('/')
 def index(request):
