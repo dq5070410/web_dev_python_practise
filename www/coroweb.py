b0c5078ef0ad1f29f0617a4b77dd9fff921896c0
yanghang <dq5070410@126.com>
2017-04-21 21:45:30 +0800

    startswith

--- www/coroweb.py
+++ www/coroweb.py
@@ -96,12 +96,12 @@ class RequestHander(object):
 				if not request.content_type:
 					return web.HTTPBadRequest('Missing Content-Type')
 				ct = request.content_type.lower()
-				if ct.startwith('application/json'):
+				if ct.startswith('application/json'):
 					params = yield from request.json()
 					if not isinstance(params,dict):
 						return web.HTTPBadRequest('JSON body must be object.')
 					kw = params
-				elif ct.startwith('application/x-www-form-urlencoded') or ct.startwith('multipart/form-data'):
+				elif ct.startswith('application/x-www-form-urlencoded') or ct.startwith('multipart/form-data'):
 					params = yield from request.post()
 					kw = dict(**params)
 				else:
