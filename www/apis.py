0ec60aad3c969dfb6881dd263298f8944b517a87
yanghang <dq5070410@126.com>
2017-04-23 23:33:39 +0800

    log list

--- www/apis.py
+++ www/apis.py
@@ -8,6 +8,30 @@ JSON API definition
 '''
 import json,logging,inspect,functools
 
+class Page(object):
+	'''
+	Page object for display pages.
+	'''
+	def __init__(self,item_count,page_index=1,page_size=0):
+		self.item_count = item_count
+		self.page_size = page_size
+		self.page_count = item_count // page_size + (1 if item_count % page_size > 0 else 0)
+		if(item_count == 0) or (page_index > self.page_count):
+			self.offset = 0
+			self.limit = 0
+			self.page_index = 1
+		else:
+			self.page_index = page_index
+			self.offset = self.page_size * (page_index - 1)
+			self.limit = self.page_size
+		self.has_next = self.page_index < self.page_count
+		self.has_previout = self.page_index > 1
+
+		def __str__(self):
+			return 'item count :%s,page_count: %s,page_size: %s,offset:%s,limit:%s' % (self.item_count,self.page_count, self.page_index, self.page_size, self.offset, self.limit)
+
+		__repr__ = __str__
+
 class APIError(Exception):
 	'''
 	the base API error which contains error(requred),data(optional),and message(optional)
